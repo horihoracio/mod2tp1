9e4b2a2d3a8a428731dfcddd62e4e37848c7a404
Hori <horacio.maldonado2@gmail.com>
2023-02-09 21:06:14 -0300

    actualizacion de tarea

---
+++ js/index.js
@@ -0,0 +1,40 @@
+//ejercicio 1
+//Crear una variable de tipo constante cuyo valor sea el siguiente mensaje “Fin del Ejercicio”
+const variable = "Fin del ejercicio"; 
+console.log(variable);
+
+//ejercicio 2
+//Declarar 5 variables de tipo string y concatenarlas en diferente orden 
+//(no importa que la oración formada no tenga sentido)
+const dato1="perro ";
+const dato2="mariachi ";
+const dato3="muertos ";
+const dato4="musica ";
+const dato5="coco";
+console.log(dato1 + dato2 + dato3 + dato4 +"y "+ dato5);
+
+//ejercicio 3
+//Una persona está intentando comprar sus productos en el super, la lista consta de frutas, 
+//verduras, carne, comida para su mascota, papel higienico y un paquete de yerba. 
+//Declare 5 variables que sean números decimales y realice la sumatoria de todos los productos, 
+//devolviendo el resultado final al cliente. 
+const frutas=100.25;
+const verduras=59.37;
+const carne=257.64;
+const comida_para_la_mascota=91.52;
+const papel_higienico=66.78;
+const paquete_de_yerba=105.81;
+
+//a) Aplicar un descuento del 10% a los productos para mascotas e imprimir el nuevo monto 
+//a abonar al cliente.
+const descuento=1.1;
+
+
+//b) Aplicar un recargo de el 18% total del valor de la compra total por pago con tarjeta 
+//de crédito, imprimir en consola el monto total a pagar y el 
+//precio por cuota (ya que el cliente desea abonar en 3 cuotas)
+const recargo=1.18;
+
+//c) Un amigo del cliente llegó al super y decidió abonar la mitad del monto total 
+//en efectivo. Al recibir la mitad en efectivo, sólo se deberá aplicar el recargo 
+//del 18% al total de la mitad del producto. Imprimir por consola el monto nuevo a abonar y el precio por cuota.
